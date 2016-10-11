1d1f887dabb05131bdb6056a50972e252ce462cc
Tristan Barrett <1006459@student.sae.edu.au>
2016-10-11 14:47:39 +1000

    character controller made and character now moves and looks around with mouse on screen
    character doesn't interact with the object the way I want it to

---
+++ Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstPersonController : MonoBehaviour {
+
+    CharacterController cc;
+
+    public float movementSpeed = 10f;
+    public float mouseSense = 5f;
+
+    private float verticalRotation = 0f;
+    public float verticalRange = 60f;
+
+	// Use this for initialization
+	void Start () {
+        cc = GetComponent<CharacterController>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Rotation
+        float rotLeftRight = Input.GetAxis("Mouse X") * mouseSense;
+        transform.Rotate(0, rotLeftRight, 0);
+
+        float rotUpDown = Input.GetAxis("Mouse Y") * mouseSense;
+
+        verticalRotation -= Input.GetAxis("Mouse Y") * mouseSense;
+        verticalRotation = Mathf.Clamp(verticalRotation, -verticalRange, verticalRange);
+
+        Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+
+        //Movement
+        float forwardSpeed = Input.GetAxis("Vertical") * movementSpeed;
+        float sideSpeed = Input.GetAxis("Horizontal") * movementSpeed;
+
+        Vector3 speed = new Vector3(sideSpeed, 0, forwardSpeed);
+        speed = transform.rotation * speed;
+
+        cc.SimpleMove(speed);
+	}
+}
